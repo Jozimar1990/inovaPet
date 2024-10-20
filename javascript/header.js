cface1063d32ad8648596fe1390a5264b3c70a49
Jozimar1990 <jozimar.1990@gmail.com>
2024-10-20 12:54:48 -0400

    Criados os arquivos .js para a header e o footer

---
+++ javascript/header.js
@@ -0,0 +1,36 @@
+const header = document.querySelector("header");
+const h2 = document.createElement("h2");
+const a = document.createElement("a");
+const paginas = [
+    {
+        titulo: "Página Inicial",
+        href: "index.html"
+    },
+    {
+        titulo: "Planos e Serviços",
+        href: "Planos_e_Serviços.html"
+    },
+    {
+        titulo: "Sobre Nós",
+        href: "sobre_nos.html"
+    },
+    {
+        titulo: "Minha Conta",
+        href: "Minha_conta.html"
+    }
+];
+
+paginas.map((el, i)=>{
+    const a = document.createElement("a");
+    a.setAttribute("href", paginas[i].href)
+    a.innerHTML = paginas[i].titulo
+    h2.appendChild(a)
+});
+header.appendChild(h2);
+
+
+
+/* <a href="index.html">Página inicial</a>
+        <a href="Planos_e_Serviços.html">Planos e Serviços</a>
+        <a href="sobre_nos.html">Sobre nós</a>
+        <a href="Minha_conta.html">Minha Conta</a> */
